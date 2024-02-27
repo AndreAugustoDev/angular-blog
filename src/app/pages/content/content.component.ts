fe87073f8906506bdbd6142b929faa1fcee50a5d
AndreAugustoDev <andreaugustoqueiroz999@gmail.com>
2024-02-26 21:36:50 -0300

    add dynamic content

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import { mock } from "../../data/mock";
 
 @Component({
 	selector: "app-content",
@@ -8,8 +9,28 @@ import { RouterLink } from "@angular/router";
 	templateUrl: "./content.component.html",
 	styleUrl: "./content.component.css",
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
 	contentImage!: string;
 	contentTitle!: string;
 	contentDescription!: string;
+
+	private id: number | null = 0;
+
+	constructor(private route: ActivatedRoute) {}
+	ngOnInit(): void {
+		this.route.paramMap.subscribe((params) => {
+			const newId = params.get("id");
+			this.id = parseInt(newId ?? "1");
+		});
+
+		this.setComponentData(this.id);
+	}
+
+	setComponentData(id: number | null) {
+		const result = mock.filter((article) => article.id === id)[0];
+
+		this.contentTitle = result.title;
+		this.contentDescription = result.description;
+		this.contentImage = result.image;
+	}
 }
